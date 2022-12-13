6709590ea223da23b7d8a0095296ad8430f5dc57
matheus-liber <matheuslibergt@gmail.com>
2022-12-13 15:46:34 -0300

    inserindo função "game over"

--- app/validacao.js
+++ app/validacao.js
@@ -1,9 +1,18 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
-    const numero = parseInt(+chute);
+    const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido</div>';
-        return;
+        if (chute === "game over") {
+            `
+            <h2>Game Over!!!</h2>
+            <h3>Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "black";
+        } else {       
+            elementoChute.innerHTML += '<div>Valor inválido</div>';
+            return;
+        }
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
